d26352a1f147204081c0c0bca751c0d41fb6f848
pranavakkian <pranavakkian@gmail.com>
2020-05-08 23:35:39 +0530

    initial commit from react-app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+/* istanbul ignore file */
+/* eslint-disable react/jsx-filename-extension */
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
+  document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({
+  onUpdate: (registration) => {
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+    window.location.reload();
+  },
+});
